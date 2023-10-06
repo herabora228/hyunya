c983ad692234d1bc442e659ccaab44a86dbc12ca
herabora228 <147203981+herabora228@users.noreply.github.com>
2023-10-06 22:20:36 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,11 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "08.09.2003"; // Дата рождения
+var fullname = "Чумазко Алексiй Дмитрович" // ФИО
+var name = "Алексiй" // Имя
+
+// Паспорт
+var passport_id = "739275497" // Номер паспорта
